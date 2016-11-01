c9f242ca586ac5797c3cb905f42c5e52d566aa62
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-02 00:49:49 +0100

    Use sendmail transport as default, make this configurable

--- src/Pckg/Mail/Service/Mail.php
+++ src/Pckg/Mail/Service/Mail.php
@@ -7,6 +7,7 @@ use Swift_Mailer;
 use Swift_MailTransport;
 use Swift_Message;
 use Swift_Mime_SimpleMessage;
+use Swift_NullTransport;
 use Swift_SendmailTransport;
 
 class Mail
@@ -24,16 +25,23 @@ class Mail
 
     public function __construct()
     {
-        /**
-         * @T00D00 ... transport should be defined by environment ...
-         *         Production environment should be handled with Sendmail or Mail transport
-         *         Dev environment should be handled with File transport
-         *         Test environment should be handled with Test/Null transport
-         */
-        $transport = new Swift_MailTransport();
-        $transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
-        $this->mailer = new Swift_Mailer($transport);
+        $this->mailer = new Swift_Mailer($this->getTransport());
         $this->mail = $this->mailer->createMessage();
+    }
+
+    public function getTransport()
+    {
+        $transportClass = config('pckg.mail.swift.transport', Swift_SendmailTransport::class);
+
+        if ($transportClass == Swift_MailTransport::class) {
+            return new Swift_MailTransport();
+
+        } else if ($transportClass == Swift_NullTransport::class) {
+            return new Swift_NullTransport();
+
+        }
+
+        return new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
     }
 
     public function from($email, $name = null)
