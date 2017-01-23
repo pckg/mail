06a4139bf04138266b9faa69fdc78bab5b6e16a9
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-01-23 18:14:01 +0100

    Remove newsletter

--- src/Pckg/Mail/Controller/Mailchimp.php
+++ src/Pckg/Mail/Controller/Mailchimp.php
@@ -1,77 +1,11 @@
 <?php namespace Pckg\Mail\Controller;
 
-use Derive\Newsletter\Entity\Newsletters;
-use Derive\Newsletter\Entity\NewslettersUsers;
-use Derive\Newsletter\Record\Newsletter;
-use Derive\Newsletter\Record\NewslettersUser;
-use Derive\Offers\Record\Offer;
-use Derive\Orders\Entity\Users;
-use Derive\Orders\Record\User;
-
 class Mailchimp
 {
 
     public function getEnewsAction()
     {
         return view('Pckg\Mail:mailchimp\enews');
-    }
-
-    public function getEnewsOfferAction(Offer $offer)
-    {
-        return view('Pckg\Mail:mailchimp\enewsoffer', ['offer' => $offer]);
-    }
-
-    public function postEnewsAction(Offer $offer = null)
-    {
-        $email = post('email');
-        $name = post('name');
-
-        $user = (new Users())->where('email', $email)->oneOr(
-            function() use ($email, $name) {
-                return User::create(
-                    [
-                        'email' => $email,
-                        'name'  => $name,
-                    ]
-                );
-            }
-        );
-
-        $newsletter = (new Newsletters())->where('type', 'default')->one();
-        if ($offer) {
-            $newsletter = (new Newsletters())->where('offer_id', $offer->id)->oneOr(
-                function() use ($offer) {
-                    return Newsletter::create(
-                        [
-                            'offer_id' => $offer->id,
-                            'title'    => 'Newsletter for offer ' . $offer->title,
-                        ]
-                    );
-                }
-            );
-        }
-
-        if ($newsletter) {
-            $newslettersUser = (new NewslettersUsers())->where('newsletter_id', $newsletter->id)
-                                                       ->where('user_id', $user)
-                                                       ->oneOr(
-                                                           function() use ($newsletter, $user) {
-                                                               return NewslettersUser::create(
-                                                                   [
-                                                                       'newsletter_id' => $newsletter->id,
-                                                                       'user_id'       => $user->id,
-                                                                       'created_at'    => date('Y-m-d H:i:s'),
-                                                                   ]
-                                                               );
-                                                           }
-                                                       );
-        }
-
-        return response()->respondWithSuccess(
-            [
-                'text' => 'Success',
-            ]
-        );
     }
 
 }
