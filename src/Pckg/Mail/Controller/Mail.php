21870646abd082b6ec60145cd8b19c9b67b620f9
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-06-05 15:10:45 +0200

    Simplify

--- src/Pckg/Mail/Controller/Mail.php
+++ src/Pckg/Mail/Controller/Mail.php
@@ -57,9 +57,7 @@ class Mail
         $offers = $this->post('offers');
         $offersHtml = null;
         if ($offers) {
-            $offersHtml = view('Pckg/Mail:offers', [
-                'offers' => (new Offers())->where('id', $offers)->all(),
-            ])->autoparse();
+            $offersHtml = (new Offers())->where('id', $offers)->all()->map('upsellHtml')->implode();
         }
 
         $recipients->each(
