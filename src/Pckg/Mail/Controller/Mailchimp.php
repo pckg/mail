8af0422e9a2be87c5d59f7c99ed79a79b68b2ca9
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-10-31 15:58:38 +0100

    Mailchimo

--- src/Pckg/Mail/Controller/Mailchimp.php
+++ src/Pckg/Mail/Controller/Mailchimp.php
@@ -1,15 +1,17 @@
 <?php namespace Pckg\Mail\Controller;
 
 use Derive\Newsletter\Controller\Newsletter;
+use Pckg\Generic\Record\Content;
 use Pckg\Mail\Form\MailchimpEnews;
 
 class Mailchimp
 {
 
-    public function getEnewsAction(MailchimpEnews $mailchimpEnewsForm)
+    public function getEnewsAction(MailchimpEnews $mailchimpEnewsForm, Content $content = null)
     {
         return view('Pckg/Mail:mailchimp/enews', [
             'enewsForm' => $mailchimpEnewsForm,
+            'content'   => $content,
         ]);
     }
 
