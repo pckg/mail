bac5328f0391c53b2baa7cf105a9f4037fe55250
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-11-29 14:50:59 +0100

    Remove gnp.si

--- src/Pckg/Mail/Console/SendMail.php
+++ src/Pckg/Mail/Console/SendMail.php
@@ -82,7 +82,7 @@ class SendMail extends Command
         /**
          * Skip dummy email.
          */
-        if (is_object($user) && is_string($user->getEmail()) && strpos($user->getEmail(), '@gnp.si')) {
+        if (is_object($user) && is_string($user->getEmail()) && strpos($user->getEmail(), '@') === false) {
             $this->output('Skipping ' . $user->getEmail());
 
             return;
